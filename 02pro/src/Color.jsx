aad331813e6daa28d317720ca955f030f1a4a15f
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2025-11-01 15:54:24 -0400

    added color

---
+++ 02pro/src/Color.jsx
@@ -0,0 +1,4 @@
+function Color() {
+  return <div style={{ color: 'blue' }}>This is a blue color text.</div>;
+}
+export default Color;
--- 02pro/src/main.jsx
+++ 02pro/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Button from './Button.jsx';
+import Color from './Color.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Button />
+    <Color />
   </StrictMode>,
 )
