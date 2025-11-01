8b98621dc37d492f6cf8a951f5212968adef701e
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2025-11-01 15:57:15 -0400

    added href

--- 02pro/src/Color.jsx
+++ 02pro/src/Color.jsx
@@ -1,4 +1,5 @@
 function Color() {
-  return <div style={{ color: 'blue' }}>This is a blue color text.</div>;
+  return (<><div style={{ color: 'blue' }}>This is a blue color text.</div>
+  <a href="https://www.google.com">google</a></>)
 }
 export default Color;
