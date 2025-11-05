32c7e34c874eaf9e1862412785ded45b883cf24d
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2025-11-05 15:44:29 -0500

    added bootstrap

--- 01base/src/main.jsx
+++ 01base/src/main.jsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Button from './Button.jsx';
-import Color from './Color.jsx';  
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Button />
-    <Color />
   </StrictMode>,
 )
