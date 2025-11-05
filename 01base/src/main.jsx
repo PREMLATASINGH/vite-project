f5e2d8e8eff839a90609e6b3eaf14ef8858b0db8
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2025-11-05 16:17:53 -0500

    added model

--- 01base/src/main.jsx
+++ 01base/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Model from './Model.jsx';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Model />
   </StrictMode>,
 )
