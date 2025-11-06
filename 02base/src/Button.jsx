44c53bafd1d8c84159c2b66b3fe55f5e5456b612
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2025-11-06 15:36:57 -0500

    added

--- 02base/src/Button.jsx
+++ 02base/src/Button.jsx
@@ -4,7 +4,7 @@ function Button() {
   
 </div>
 <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-  <button type="button" class="btn btn-danger">Left</button>
+  <button type="button" class="btn btn-secondary">Left</button>
   <button type="button" class="btn btn-warning">Middle</button>
   <button type="button" class="btn btn-success">Right</button>
 </div>
