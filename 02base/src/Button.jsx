e173fadddd17029b5932c8788ba579284b56d494
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2025-11-06 15:31:31 -0500

    added link

--- 02base/src/Button.jsx
+++ 02base/src/Button.jsx
@@ -1,5 +1,8 @@
 function Button() {
-  return (<>
+  return (<><div class="btn-group">
+  <a href="https://www.google.com"class="btn btn-primary active" aria-current="page">Active link</a>
+  
+</div>
   <button className="btn btn-primary">Click Me</button></>)
   ;
 }
